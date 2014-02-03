bec3458349411fcbb1c39b4edbefbe88c7fa7f2b
timii <paananen.t@gmail.com>
2014-02-03 22:19:07 +0200

    - Initial commit

---
+++ src/main/java/net/vksn/jenkins/plugins/hideable/HideableStringParameterDefinition.java
@@ -0,0 +1,54 @@
+package net.vksn.jenkins.plugins.hideable;
+
+import hudson.Extension;
+import hudson.model.ParameterValue;
+import hudson.model.StringParameterDefinition;
+import net.sf.json.JSONObject;
+import net.sf.json.util.JSONBuilder;
+import org.jvnet.solaris.libzfs.jna.vdev_aux_t;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Created by timii on 2/3/14.
+ */
+public class HideableStringParameterDefinition extends StringParameterDefinition {
+
+    @DataBoundConstructor
+    public HideableStringParameterDefinition(String name, String value, String description, boolean hidden) {
+        super(name, value, description);
+        this.hidden = hidden;
+    }
+
+    private boolean hidden;
+
+    @Extension
+    public static class DefinitionDescriptor extends DescriptorImpl {
+
+        @Override
+        public String getDisplayName() {
+            return "Hideable String Parameter";
+        }
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req, JSONObject jsonObject) {
+        HideableStringParameterValue value = req.bindJSON(HideableStringParameterValue.class, jsonObject);
+        value.setHidden(isHidden());
+        return value;
+    }
+
+    @Override
+    public ParameterValue createValue(String value) {
+        return new HideableStringParameterValue(getName(), value, getDescription(), isHidden());
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+}
---
+++ src/main/java/net/vksn/jenkins/plugins/hideable/HideableStringParameterValue.java
@@ -0,0 +1,25 @@
+package net.vksn.jenkins.plugins.hideable;
+
+import hudson.model.StringParameterValue;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Created by timii on 2/3/14.
+ */
+public class HideableStringParameterValue extends StringParameterValue {
+
+    private boolean hidden;
+
+
+    @DataBoundConstructor
+    public HideableStringParameterValue(String name, String value, String description, boolean hidden) {
+        super(name,value, description);
+        this.hidden = hidden;
+    }
+
+    public boolean getHidden() { return hidden; }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+}
